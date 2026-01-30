259c446957ce2cec0c199d9973ccc8102066e832
Mateus A. Duarte <almeidaduartemateus7@gmail.com>
2026-01-30 16:05:58 -0300

    Liçãao_9_conversão_do_real

---
+++ conversor_de_moedas.py
@@ -0,0 +1,5 @@
+real = float(input('Digite quantos reais tem em sua carteira R$'))
+dolar = real / 5.27
+euro = real / 6.25
+iene = real / 29.37
+print('Com {:.2f}R$ você pode comprar US{:.2f}, {:.2f}€, {:.2f}¥'.format(real,dolar,euro,iene))
