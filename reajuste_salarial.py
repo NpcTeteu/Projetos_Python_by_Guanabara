e35c3d0adfb35b73f72c09b2dc2f4666300442aa
Mateus A. Duarte <almeidaduartemateus7@gmail.com>
2026-02-28 15:04:01 -0300

    Mudança de nome

--- reajuste_salarial.py
+++
@@ -1,3 +0,0 @@
-sala = float(input('Qual é o salário do funcionário? R$'))
-aumento = sala + (sala*0.15)
-print('Um funcionário ganhava R${:.2f}, com o aumento de 15%, passa a receber {:.2f}'.format(sala,aumento))
