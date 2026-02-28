d0d5cec18a3ff13ddcae6b03726dcb0a52598c50
Mateus A. Duarte <almeidaduartemateus7@gmail.com>
2026-02-28 15:10:36 -0300

    Mudança de nome e adição de comentário

--- ex_015.py
+++ ex_015.py
@@ -1,4 +1,7 @@
+# Importa a biblioteca math, que tem módulos relacionados a matemática
 import math
 num = float(input("Digite um numero: "))
+# O floor arredonda para baixo o número com vírgula
 arre = math.floor(num)
 print("O numero {} tem a parte inteira {}".format(num,arre))
+
