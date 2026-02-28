b53f4c1713e827b928f8ad797ace5a14172d765f
Mateus A. Duarte <almeidaduartemateus7@gmail.com>
2026-02-28 15:21:00 -0300

    Introdução aos Módulos

--- teste_01.py
+++ teste_01.py
@@ -1,4 +1,6 @@
 import math
 num = int(input("Digite um número "))
+# módulo que faz a raiz quadrada ao ínves de fazer uma potência de 0,5 do número
 raiz = math.sqrt(num)
+
 print("A raiz de {:.0f} é igual a {:.0f}".format(num,math.floor(raiz)))
