e21b360a906a431456896be0c8cc1ebba2c7effb
Mateus A. Duarte <almeidaduartemateus7@gmail.com>
2026-02-28 14:45:01 -0300

    Mudança de nome e adição de comentário

--- antecessor_sucessor.py
+++
@@ -1,2 +0,0 @@
-user_num = int(input("Digite um numero: "))
-print("O sucessor desse numero é {} \n E o antecessor é {}".format(user_num+1,user_num-1))
---
+++ ex_005.py
@@ -0,0 +1,4 @@
+user_num = int(input("Digite um numero: "))
+
+# O \n faz acontecer um quebra de linha, e confome a ordem que se coloca no format é oque substitui as chaves.
+print("O sucessor desse numero é {} \n E o antecessor é {}".format(user_num+1,user_num-1))
